8cba6dbfb6c4961ae9c3f928917cda787592931e
SohamE <62288128+SohamE@users.noreply.github.com>
2025-08-16 12:43:43 +0530

    Create Container With Most Water.java

---
+++ Two Pointers/Container With Most Water.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int l = 0;
+        int r = height.length - 1;
+        int area = 0;
+
+        while (l < r) {
+            area = Math.max(area, (r - l) * Math.min(height[l], height[r]));
+            if (height[l] <= height[r]) {
+                l++;
+                continue;
+            }
+            if (height[r] < height[l]) {
+                r--;
+                continue;
+            }
+        }
+        return area;
+    }
+}

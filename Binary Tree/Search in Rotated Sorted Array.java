b9aa474a3af5934f4d697c086c1ddf5eab6dde43
SohamE <62288128+SohamE@users.noreply.github.com>
2025-08-17 10:29:20 +0530

    Update Search in Rotated Sorted Array.java

--- Binary Tree/Search in Rotated Sorted Array.java
+++ Binary Tree/Search in Rotated Sorted Array.java
@@ -7,19 +7,24 @@ class Solution {
             int m = (l + r) / 2;
             if (nums[m] == target)
                 return m;
+            // If the whole segment is sorted, apply simple binary tree
             if (nums[l] < nums[r]) {
                 if (target > nums[m])
                     l = m + 1;
                 if (target < nums[m])
                     r = m - 1;
             } else {
+                // if left segment is sorted
                 if (nums[m] >= nums[l]) {
+                    // if the target falls in the left segment, make l & r to that.
                     if (target < nums[m] && target >= nums[l]) {
                         r = m - 1;
                     } else {
                         l = m + 1;
                     }
+                // right segment is sorted.
                 } else {
+                    // if the target falls in right segment, make l & r to that.
                     if (target > nums[m] && target <= nums[r]) {
                         l = m + 1;
                     } else {

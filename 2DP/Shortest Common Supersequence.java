c27a261f1d6e6a6fc0b215a71aaa8808686d29e7
SohamE <62288128+SohamE@users.noreply.github.com>
2025-09-06 23:16:53 +0530

    Create Shortest Common Supersequence.java

---
+++ 2DP/Shortest Common Supersequence.java
@@ -0,0 +1,49 @@
+class Solution {
+    public String shortestCommonSupersequence(String s1, String s2) {
+        int n = s1.length();
+        int m = s2.length();
+        int[][] dp = new int[n + 1][m + 1];
+        dp[n][m] = 0;
+        dp[n - 1][m] = 0;
+        dp[n][m - 1] = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = m - 1; j >= 0; j--) {
+                if (s1.charAt(i) == s2.charAt(j)) {
+                    dp[i][j] = 1 + dp[i + 1][j + 1];
+                } else {
+                    dp[i][j] = Math.max(dp[i + 1][j], dp[i][j + 1]);
+                }
+            }
+        }
+
+        int i = 0;
+        int j = 0;
+        String s = "";
+        while (i < n && j < m) {
+            if (i < n && j < m && s1.charAt(i) == s2.charAt(j)) {
+                s += s1.charAt(i);
+                i++;
+                j++;
+            } else {
+                if (dp[i + 1][j] > dp[i][j + 1]) {
+                    s += s1.charAt(i);
+                    i++;
+                }
+                else {
+                    s += s2.charAt(j);
+                    j++;
+                }
+            }
+        }
+        while (i < n) {
+            s += s1.charAt(i);
+            i++;
+        }
+        while (j < m) {
+            s += s2.charAt(j);
+            j++;
+        }
+        return s;
+        
+    }
+}

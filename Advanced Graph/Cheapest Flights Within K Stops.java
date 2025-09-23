ccc8ea483f03aeaadaedeb122cbae1051c76eee7
SohamE <62288128+SohamE@users.noreply.github.com>
2025-09-23 22:11:38 +0530

    Update Cheapest Flights Within K Stops.java

--- Advanced Graph/Cheapest Flights Within K Stops.java
+++ Advanced Graph/Cheapest Flights Within K Stops.java
@@ -1,44 +1,40 @@
 class Solution {
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
-        List<List<Pair<Integer, Integer>>> adjList = getAdjList(flights, n);
-        // We are not taking priority queue based on distance, because we are using min heap on stops. As every time stops increases by 1, the queue is already a min heap of stops.
+        List<List<int[]>> adjList = findAdjList(n, flights);
+        // Any one of the below works.
+        // Queue<int[]> minHeap = new PriorityQueue<>((a, b) -> a[2] - b[2]);
         Deque<int[]> minHeap = new ArrayDeque<>();
         int[] distArr = new int[n];
         Arrays.fill(distArr, Integer.MAX_VALUE);
-        // Initializing the start
         distArr[src] = 0;
-        minHeap.offer(new int[] { 0, src, 0 });
+        minHeap.add(new int[] { 0, src, 0 });
         while (!minHeap.isEmpty()) {
             int[] pick = minHeap.poll();
             int dist = pick[0];
             int node = pick[1];
             int stops = pick[2];
-            for (Pair<Integer, Integer> adjNode : adjList.get(node)) {
-                int key = adjNode.getKey();
-                int value = adjNode.getValue();
-                if (dist + value < distArr[key] && stops <= k) {
-                    distArr[key] = dist + value;
-                    minHeap.offer(new int[] { distArr[key], key, stops + 1 });
+            for (int[] adjNode : adjList.get(node)) {
+                if (dist + adjNode[1] < distArr[adjNode[0]] && stops <= k) {
+                    distArr[adjNode[0]] = dist + adjNode[1];
+                    minHeap.offer(new int[] { distArr[adjNode[0]], adjNode[0], stops + 1 });
                 }
-
             }
         }
         return distArr[dst] == Integer.MAX_VALUE ? -1 : distArr[dst];
-
     }
 
-    public List getAdjList(int[][] flights, int n) {
-        List<List<Pair<Integer, Integer>>> adjList = new ArrayList<>();
+    public List<List<int[]>> findAdjList(int n, int[][] flights) {
+        List<List<int[]>> adjList = new ArrayList<>();
         for (int i = 0; i < n; i++)
             adjList.add(new ArrayList<>());
-
         for (int i = 0; i < flights.length; i++) {
-            int src = flights[i][0];
-            int dest = flights[i][1];
-            int price = flights[i][2];
-            adjList.get(src).add(new Pair<>(dest, price));
+            int[] flight = flights[i];
+            adjList.get(flight[0]).add(new int[] { flight[1], flight[2] });
         }
-
         return adjList;
     }
 }
+
+// *** we cannot use minheap on distance in this problem as if we can reach the destination by taking large edge with all stops, rather than small edge with less stop.
+//     hence we have to use priority queue on steps
+//     or we can use a simple array deque as the steps is always increasing order when we add in the minheap

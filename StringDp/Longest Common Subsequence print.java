c9938a40d52b6b77abd71fc28c06fb3d04a84fa5
SohamE <62288128+SohamE@users.noreply.github.com>
2025-11-08 01:08:10 +0530

    Update Longest Common Subsequence print.java

--- StringDp/Longest Common Subsequence print.java
+++ StringDp/Longest Common Subsequence print.java
@@ -1,41 +1,43 @@
 class Solution {
-    public int longestCommonSubsequence(String text1, String text2) {
+    public String shortestCommonSupersequence(String text1, String text2) {
         int[][] dp = new int[text1.length()][text2.length()];
         int n = text1.length();
         int m = text2.length();
         for (int i = 0; i < text1.length(); i++)
             Arrays.fill(dp[i], -1);
+        int ans = longestCommonSubsequence(text1, text2, 0, 0, dp);
         String lcs = "";
-        int i = n - 1;
-        int j = m - 1;
-        while (i >= 0 && j >= 0) {
-                if (text1.charAt(i) == text2.charAt(j)) {
-                    lcs += text1.charAt(i);
-                    i--;
-                    j--;
+        int i = 0;
+        int j = 0;
+        while (i < n && j < m) {
+            if (text1.charAt(i) == text2.charAt(j)) {
+                lcs += text1.charAt(i);
+                i++;
+                j++;
+            } else {
+                int up = (i < n - 1) ? dp[i + 1][j] : 0;
+                int left = (j < m - 1) ? dp[i][j + 1] : 0;
+                if (up >= left) {
+                    i++;
                 } else {
-                    if (i < 1 || j < 1)
-                        break;
-                    if (dp[i][j - 1] > dp[i - 1][j]) {
-                        j--;
-                    } else {
-                        i--;
-                    }
+                    j++;
                 }
+            }
         }
-      return lcs;
+        return lcs;
+
     }
 
-    public int dfs(String a, String b, int i, int j, int[][] dp) {
+    public int longestCommonSubsequence(String a, String b, int i, int j, int[][] dp) {
         if (i > a.length() - 1 || j > b.length() - 1)
             return 0;
         if (dp[i][j] != -1)
             return dp[i][j];
         int res = 0;
         if (a.charAt(i) == b.charAt(j)) {
-            res = 1 + dfs(a, b, i + 1, j + 1, dp);
+            res = 1 + longestCommonSubsequence(a, b, i + 1, j + 1, dp);
         } else {
-            res = Math.max(dfs(a, b, i + 1, j, dp), dfs(a, b, i, j + 1, dp));
+            res = Math.max(longestCommonSubsequence(a, b, i + 1, j, dp), longestCommonSubsequence(a, b, i, j + 1, dp));
         }
         dp[i][j] = res;
         return res;

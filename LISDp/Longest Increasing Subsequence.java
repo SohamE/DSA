886cef0b1bc69f7b945755cc81c2453df72fac37
SohamE <62288128+SohamE@users.noreply.github.com>
2025-11-09 00:03:10 +0530

    Update Longest Increasing Subsequence.java

--- LISDp/Longest Increasing Subsequence.java
+++ LISDp/Longest Increasing Subsequence.java
@@ -57,7 +57,37 @@ class Solution {
         }
         return ans;
     }
-    
 
+    // Print LIS
+    public int[] printLIS(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n];
+        int[] back = new int[n];
+        int lastIndex = 0;
+        int ans = 1;
+        Arrays.fill(dp, 1);
+        for (int k = 0; k < n; k++) {
+            back[k] = k;
+            for (int prev = 0; prev < k; prev++) {
+                if (nums[prev] < nums[k]) {
+                    if (1 + dp[prev] > dp[k]) {
+                        dp[k] = 1 + dp[prev];
+                        back[k] = prev;
+                    }
+                }
+            }
+            if (dp[k] > ans) {
+                ans = dp[k];
+                lastIndex = k;
+            }
+        }
+        int[] store = new int[n];
+        for (int i = ans -1;i>=0;i--) {
+            store[i] = nums[lastIndex];
+            System.out.println(store[i]);
+            lastIndex = back[lastIndex];
+        }
+        return store;
+    }
     
 }
